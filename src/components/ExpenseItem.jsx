0b59fadcc8589afc4c56276dd61009d504c6ad4e
Luigi T <luigitejada@yahoo.com>
2022-12-06 16:09:22 -0500

    add expenseDate component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,13 +1,14 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = ({ title, amount, date }) => {
   
 
   return (
-    <div className='expense-item'>
-      <div>{date.toUTCString()}</div>
-      <div className='expense-item__description'>
-        <h2>{ title }</h2>
-        <div className='expense-item__price'>${ amount }</div>
+    <div className="expense-item">
+      <ExpenseDate date={date}/>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
