434c68a63e3d9ba58782259e0d63beee69650d47
Luigi T <luigitejada@yahoo.com>
2022-12-12 13:14:28 -0500

    edit react structure

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,5 +1,5 @@
 import './Card.css'
-const Card = ({className , children}) => {
+const Card = ({ className , children }) => {
   const classes = 'card ' + className
   return ( 
     <div className={classes}>{children}</div>
